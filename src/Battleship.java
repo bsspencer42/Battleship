ed958b109aa41271603e6f13ddd5b8690d08de4d
bsspencer42 <bsspencer42@gmail.com>
2021-07-11 11:20:12 -0500

    Add game function. No board print during game.

--- src/Battleship.java
+++ src/Battleship.java
@@ -10,33 +10,33 @@ public class Battleship {
         Scanner input = new Scanner(System.in); // Create scanner object
 
         // Setup player boards
-        char[][] player1 = new char[][] {
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
+        char[][] player1 = new char[][]{
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
         };
-        char[][] player2 = new char[][] {
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
+        char[][] player2 = new char[][]{
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
         };
-        char[][] player1TargetHist = new char[][] {
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
+        char[][] player1TargetHist = new char[][]{
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
         };
-        char[][] player2TargetHist = new char[][] {
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
-            {'-', '-', '-', '-', '-'},
+        char[][] player2TargetHist = new char[][]{
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
+                {'-', '-', '-', '-', '-'},
         };
 
         // BOARD SETUP
@@ -107,11 +107,11 @@ public class Battleship {
                 continue;
             }
             // Check if position already has ship
-            if (player2[xCoord + 1][yCoord + 1] == '@') {
+            if (player2[xCoord][yCoord] == '@') {
                 System.out.println("You already have a ship there. Choose different coordinates.");
                 continue;
             } else {
-                player2[xCoord + 1][yCoord + 1] = '@';
+                player2[xCoord][yCoord] = '@';
             }
 
             // Increment counter
@@ -120,7 +120,70 @@ public class Battleship {
         printBattleShip(player2);
 
         // Play game
-        System.out.println("\nPlayer 1, enter hit row/column:");
+        // Hit counters
+        int player1HitCount = 0;
+        int player2HitCount = 0;
+        int[] attackCoord = {-1, -1};
+        int playerCoordValid;
+        int attackResult;
+
+        // Begin game
+        while (player1HitCount < 5 && player2HitCount < 5) {
+            // Player 1 turn
+
+            // Validate target coordinates
+            playerCoordValid = 0;
+            while (playerCoordValid == 0) {
+                System.out.println("Player 1, enter hit row/column:");
+                attackCoord = checkCoord(input, player2, player1TargetHist);
+                if (attackCoord[0] < 0 || attackCoord[1] < 0) {
+                    continue;
+                }
+                playerCoordValid = 1;
+            } // Check if attack coords valid, loop back if not
+
+            // Update boards
+            attackResult = updateBoards(player2, player1TargetHist, attackCoord);
+
+            // Print result
+            if (attackResult == 0) {
+                System.out.println("PLAYER 1 MISSED!");
+            } else {
+                System.out.println("PLAYER 1 HIT PLAYER 2’s SHIP!");
+                player1HitCount++;
+            }
+
+            // Player 2 turn
+
+            // Validate target coordinates
+            playerCoordValid = 0;
+            while (playerCoordValid == 0) {
+                System.out.println("Player 2, enter hit row/column:");
+                attackCoord = checkCoord(input, player1, player2TargetHist);
+                if (attackCoord[0] < 0 || attackCoord[1] < 0) {
+                    continue;
+                }
+                playerCoordValid = 1;
+            } // Check if attack coords valid, loop back if not
+
+            // Update boards
+            attackResult = updateBoards(player1, player1TargetHist, attackCoord);
+
+            // Print result
+            if (attackResult == 0) {
+                System.out.println("PLAYER 2 MISSED!");
+            } else {
+                System.out.println("PLAYER 2 HIT PLAYER 1’s SHIP!");
+                player2HitCount++;
+            }
+        }
+
+        // End game
+        if (player1HitCount == 5) {
+            System.out.println("PLAYER 1 WINS! YOU SUNK ALL OF YOUR OPPONENT’S SHIPS!");
+        } else {
+            System.out.println("PLAYER 2 WINS! YOU SUNK ALL OF YOUR OPPONENT’S SHIPS!");
+        }
 
     }
 
@@ -140,6 +203,59 @@ public class Battleship {
             }
             System.out.println("");
         }
+    }
+
+    // Method to check if attack coordinates are valid
+    private static int[] checkCoord(Scanner input, char[][] player, char[][] playerTargetHistory) {
+        int xCoord;
+        int yCoord;
+        int[] coords;
+        if (input.hasNextInt()) {
+            xCoord = input.nextInt();
+        } else {
+            input.nextLine();
+            System.out.println("Invalid coordinates. Choose different coordinates.");
+            coords = new int[]{-1, 0};
+            return coords;
+        }
+        if (input.hasNextInt()) {
+            yCoord = input.nextInt();
+            input.nextLine();
+        } else {
+            input.nextLine();
+            System.out.println("Invalid coordinates. Choose different coordinates.");
+            coords = new int[]{xCoord, -1};
+            return coords;
+        }
+
+        // Check to see if X/Y coordinates out of bounds
+        if ((xCoord > 4 || xCoord < 0) || (yCoord > 4 || yCoord < 0)) {
+            System.out.println("Invalid coordinates. Choose different coordinates.");
+            coords = new int[]{-2, -2};
+            return coords;
+        }
+        // Check to see if already attacked position previously
+        if (playerTargetHistory[xCoord][yCoord] != '-') {
+            System.out.println("You already fired on this spot. Choose different coordinates.");
+            coords = new int[]{-3, -3};
+            return coords;
+        }
+        coords = new int[]{xCoord, yCoord};
+        return coords;
+    }
+
+    private static int updateBoards(char[][] player, char[][] playerTargetHist, int[] attackCoord) {
+        switch (player[attackCoord[0]][attackCoord[1]]) {
+            case '-':
+                player[attackCoord[0]][attackCoord[1]] = 'O';
+                playerTargetHist[attackCoord[0]][attackCoord[1]] = 'O';
+                return 0;
+            case '@':
+                player[attackCoord[0]][attackCoord[1]] = 'X';
+                playerTargetHist[attackCoord[0]][attackCoord[1]] = 'X';
+                return 1;
+        }
+        return -1;
     }
 
 }
